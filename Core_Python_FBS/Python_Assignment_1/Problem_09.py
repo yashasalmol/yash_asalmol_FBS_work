a363535abf635a4b35e6ec505bd47cf14c27d0b4
yash asalmol <153384480+yashasalmol@users.noreply.github.com>
2025-07-01 08:48:54 +0530

    Update Problem_09.py

--- Core_Python_FBS/Python_Assignment_1/Problem_09.py
+++ Core_Python_FBS/Python_Assignment_1/Problem_09.py
@@ -1,7 +1,7 @@
 # A program to enter base and height of a triangle and find its area.
 
-base = int(input("Enter a base : "))
-height = int(input("Enter a height : "))
+base = float(input("Enter a base : "))
+height = float(input("Enter a height : "))
 area = 0.5 * base * height
 # area = (1/2) * base * height
 print("The area of triangle is : ",area)
