fcdf99beceed84add2417f6fe9bc7dc817fd4988
yash asalmol <153384480+yashasalmol@users.noreply.github.com>
2025-07-01 08:27:08 +0530

    Update Problem_06.py

--- Core_Python_FBS/Python_Assignment_2/Problem_06.py
+++ Core_Python_FBS/Python_Assignment_2/Problem_06.py
@@ -4,8 +4,8 @@ basic = int(input("Enter a basic salary : "))
 
 da = basic * (10/100)
 ta = basic * (12/100)
-haa = basic * (15/100)
+hra = basic * (15/100)
 
-total_salary = basic + da + ta + haa
+total_salary = basic + da + ta + hra
 
 print("Total salary of the employee is : ",total_salary)
