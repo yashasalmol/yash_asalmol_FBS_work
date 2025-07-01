d614bbfa143817b2d505102364d6257a976d7d03
yash asalmol <153384480+yashasalmol@users.noreply.github.com>
2025-07-01 08:30:11 +0530

    Update Problem_03.py

--- Core_Python_FBS/Python_Assignment_2/Problem_03.py
+++ Core_Python_FBS/Python_Assignment_2/Problem_03.py
@@ -1,5 +1,7 @@
-feet = int(input("Enter feet :"))
-inches = int(input("Enter inches :"))
+# Convert distant given in feet and inches into meter and centimeter. 
+
+feet = float(input("Enter feet :"))
+inches = float(input("Enter inches :"))
 
 meters = (feet * 0.3048) + (inches * 0.0254)
 centimeter = meters *100
