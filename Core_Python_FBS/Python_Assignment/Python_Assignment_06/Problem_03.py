b28dff7970856b60a0d18849fc774d4bd98beedb
yash asalmol <153384480+yashasalmol@users.noreply.github.com>
2025-07-15 22:10:50 +0530

    Update Problem_03.py

--- Core_Python_FBS/Python_Assignment/Python_Assignment_06/Problem_03.py
+++ Core_Python_FBS/Python_Assignment/Python_Assignment_06/Problem_03.py
@@ -1,22 +1,13 @@
-# for i in range(1,5):
-#     for j in range(1,6-i):
-#         print(" ",end=" ")
-
-#     for j in range(1,i+1):
-#         if(i==j or j==i-4):
-#             print("1 ",end=" ")
-#         else:
-#             print(j," ",end=" ")
-#     print()
-
-for i in range(1,4):
-    for j in range(1,5-i):
+for i in range(0,4):
+    for j in range(4-i-1):
         print(" ",end=" ")
-
-    for j in range(1,i+1):
-        print(j,end=' ')
-    j = j+1
-    for j in range(1,i):
-        print(j,end=" ")
-        j+=1
+    k=1
+    for j in range(i+1):
+        print(k,end='   ')
+        k = k * (i-j)//(j+1)
     print()
+
+#         1   
+#       1   1   
+#     1   2   1
+#   1   3   3   1
